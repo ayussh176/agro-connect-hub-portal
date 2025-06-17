28899609c0ed994410e387e946b011f8a95f2a64
ayussh176 <ayushmalik852@gmail.com>
2025-06-17 15:59:11 +0530

    initial commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCroIhoSwbMFgzWZvTVmb3I9TZUTpE9zr8",
+  authDomain: "kisankhata-71990.firebaseapp.com",
+  projectId: "kisankhata-71990",
+  storageBucket: "kisankhata-71990.firebasestorage.app",
+  messagingSenderId: "582468827692",
+  appId: "1:582468827692:web:875008c4571c3f40086f89",
+  measurementId: "G-20L8PKXK7N"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
